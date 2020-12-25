266377ce8db2e323aed720fdc5dce6ea76838e7d
Russ <18russjames@gmail.com>
2020-12-24 22:49:39 -0800

    fixed registration auto login

--- src/routes/RegistrationRoute/RegistrationRoute.js
+++ src/routes/RegistrationRoute/RegistrationRoute.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react'
 import RegistrationForm from '../../components/RegistrationForm/RegistrationForm'
+import UserContext from '../../contexts/UserContext'
+import AuthApiService from '../../services/auth-api-service'
 
 class RegistrationRoute extends Component {
   static defaultProps = {
+    location: {},
     history: {
-      push: () => {},
+      push: () => { },
     },
   }
 
-  handleRegistrationSuccess = () => {
-    const { history } = this.props
-    history.push('/login')
+  static contextType = UserContext
+
+  handleRegistrationSuccess = (username, password) => {
+    AuthApiService.postLogin({
+      username: username,
+      password: password,
+    })
+      .then(res => {
+        this.context.processLogin(res.authToken)
+
+
+
+        const { location, history } = this.props
+
+
+        const destination = (location.state || {}).from || '/'
+        history.push(destination)
+      })
+      .catch(res => {
+        console.log(res.error)
+      })
   }
 
   render() {
